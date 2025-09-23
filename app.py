378cedc4a60849829abd5e11492aebaea80ea405
FarzanHussain <94126247+FarzanHussain123@users.noreply.github.com>
2025-09-23 17:41:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -236,4 +236,6 @@ def ping():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
